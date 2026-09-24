a68e3be4785260daf1c2afe0efde2e34064e898b
mkirushev <m.kirushev@yandex.ru>
2026-09-24 17:44:33 +0300

    Some_new_update

--- Semestr_1/Prog/Lab_1/Main.java
+++ Semestr_1/Prog/Lab_1/Main.java
@@ -1,9 +1,30 @@
 import java.util.Random;
-import java.lang.Math;
 import java.util.Set;
 import java.util.HashSet;
 
 public class Main {
+
+    public static double GetElement(float x, long e, Set<Integer> check){
+        if (e == 18) {
+            return Math.cbrt(0.25/(Math.cos(x) - 1));
+        } else if (check.contains(e)) {
+            return Math.pow(0.5/((1/3) + Math.asin(1/Math.exp(Math.abs(x)))), 3);
+        } else {
+            return Math.pow(Math.sin(Math.log(Math.pow(Math.abs(x) * (Math.abs(x) + 1), 2))), Math.asin(Math.sin(Math.pow(Math.exp(x), 0.75/(Math.log(Math.abs(x)))))) * (Math.sin(Math.pow((Math.exp(x)-0.25)/(Math.asin((x-1)/12)), 2)) - Math.PI));
+        }
+    }
+
+
+    public static void print(double[][] n) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 19; j++) {
+                System.out.printf("%.2f\t", n[i][j]);
+            }
+            System.out.println();
+        }      
+    }
+
+
     public static void main(String[] args) {
         long[] e = new long[10];
         int p = 0;
@@ -23,6 +44,7 @@ public class Main {
         }
 
         double[][] n = new double[10][19];
+        
         Set<Integer> check = new HashSet<>();
         for (int i = 8; i < 22; i+=2) {
             if (i == 12 || i == 18 || i == 20) {
@@ -34,20 +56,10 @@ public class Main {
 
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 19; j++) {
-                if (e[i] == 18) {
-                    n[i][j] = Math.cbrt(0.25/(Math.cos(x[j]) - 1));
-                } else if (check.contains(e[i])) {
-                    n[i][j] = Math.pow(0.5/((1/3) + Math.asin(1/Math.exp(Math.abs(x[j])))), 3);
-                } else {
-                    n[i][j] = Math.pow(Math.sin(Math.log(Math.pow(Math.abs(x[j]) * (Math.abs(x[j]) + 1), 2))), Math.asin(Math.sin(Math.pow(Math.exp(x[j]), 0.75/(Math.log(Math.abs(x[j])))))) * (Math.sin(Math.pow((Math.exp(x[j])-0.25)/(Math.asin((x[j]-1)/12)), 2)) - Math.PI));
-                }
+                n[i][j] = GetElement(x[j], e[i], check);
             }
         }
 
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 19; j++) {
-                System.out.printf("%.2f ", n[i][j]);
-            }
-        }
+        print(n);
     }
 }
